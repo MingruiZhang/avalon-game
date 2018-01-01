4fdbe82cb08381cca74f8d3d11b9e738507e911a
mzhang <mzhang@twitter.com>
2017-12-31 20:52:31 -0800

    [master] Commit No.0

--- server/server.js
+++ server/server.js
@@ -1,19 +1,21 @@
-import bodyParser from 'body-parser'
-import express from 'express'
-import path from 'path'
-const app = express()
+import bodyParser from 'body-parser';
+import express from 'express';
+import path from 'path';
+const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
+console.log('process.env.PORT: ', process.env.PORT);
+
 const PORT = process.env.PORT || 3001;
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // any routes not picked up by the server api will be handled by the react router
-const staticFiles = express.static(path.join(__dirname, '../../client/build'))
-app.use(staticFiles)
-app.use('/*', staticFiles)
+const staticFiles = express.static(path.join(__dirname, '../../client/build'));
+app.use(staticFiles);
+app.use('/*', staticFiles);
 app.set('port', PORT);
 
 // Socket.io connection
@@ -26,6 +28,6 @@ io.on('connection', client => {
   });
 });
 
-server.listen(PORT, function () {
+server.listen(PORT, function() {
   console.log('Server listening at port %d', PORT);
 });
