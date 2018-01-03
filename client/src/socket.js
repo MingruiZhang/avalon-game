35c5311dbfcbea1c203f61d58c138827d69931cb
mzhang <mzhang@twitter.com>
2018-01-02 22:37:36 -0800

    [master] Commit No.0

--- client/src/socket.js
+++ client/src/socket.js
@@ -5,4 +5,6 @@ import io from 'socket.io-client';
  * In local development, we server react and socket.io in different ports. So we need to specifically listent to :3001
  * In production, we only have one server, so using io() will get the server the client code is served.
  */
-export default (isDevelopment ? io('http://localhost:3001') : io());
+
+const getSocket = () => (isDevelopment ? io('http://localhost:3001') : io());
+export default getSocket();
